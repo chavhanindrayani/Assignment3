0b07d0425bb38584d2ac155c4f190e16df8d6447
chavhanindrayani <chavhanindrayani2002@gmail.com>
2025-04-02 12:02:43 +0530

    Add files via upload

---
+++ checkAmstrongNo.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class AmstrongNo
+{
+    public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the number");
+		int n = 153;
+
+	    int sum=0;
+		int rem=0;
+	    int copy=n;
+
+		while(n>0)
+		{
+			rem=n%10;
+			sum=rem*rem*rem+sum;
+			n=n/10;
+		}
+		if(copy == sum)
+		{
+			System.out.println("This number is amstrong");
+		}
+		else
+		{
+			System.out.println("This number is not amstrong");
+        }
+	}
+}
