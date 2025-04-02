1ca86ea48429a93bb0c0d5bee4ae85254f9315ea
chavhanindrayani <chavhanindrayani2002@gmail.com>
2025-04-02 11:59:06 +0530

    Add files via upload

---
+++ Hello_5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Hello_5
+{
+	public static void main(String args[])
+	{
+		int num = 5;
+		for(int i=1; i<=num; i++)
+		{
+			System.out.println("Hello Wrold = " +i);
+		}
+	}
+}
