a7e7f2ccaeed2b13b378063107160af43db31dcf
chavhanindrayani <chavhanindrayani2002@gmail.com>
2025-04-02 11:50:29 +0530

    Add files via upload

---
+++ oddEven.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class oddEven
+{
+	public static void main(String args[])
+	{
+		int number=9;
+
+		if(number % 2 == 0)
+		{
+			System.out.println("This is even number ");
+		}
+		else
+		{
+			System.out.println("This is odd number  ");
+		}
+	}
+}
