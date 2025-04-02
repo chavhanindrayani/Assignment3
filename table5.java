83506a266ca4086246a8a631e6984a29599528af
chavhanindrayani <chavhanindrayani2002@gmail.com>
2025-04-02 11:58:28 +0530

    Add files via upload

---
+++ table5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class table5
+{
+	public static void main(String args[])
+	{
+		int n = 5;
+		for(int i=1; i<=10; i++)
+		{
+			int table = i*n;
+			System.out.println(table);
+		}
+	}
+}
